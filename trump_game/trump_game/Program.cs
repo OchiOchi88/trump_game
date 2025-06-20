43f9da2c69ef93927889ae24b36a8ab036a61f63
OchiOchi88 <DESKTOP-GQ60U0C\student>
2025-06-20 11:22:40 +0900

    #1 対応中

--- trump_game/trump_game/Program.cs
+++ trump_game/trump_game/Program.cs
@@ -10,6 +10,25 @@ namespace trump_game
     {
         static void Main(string[] args)
         {
+            //  カード番号を4回入力させる
+            for (int i = 0; i < 4; i++)
+            {
+                //  1度の入力で範囲内の値が入力されるまで繰り返す
+                while (true)
+                {
+                    //  カード番号の入力
+                    Console.Write($"{i + 1}番目の数字を入力してください。(1～4) > ");
+
+                    //  入力内容のチェック
+                    if (!int.TryParse(Console.ReadLine(), out int input))
+                    {
+                        //  範囲外の値が入力されたらエラー表示して再入力させる
+                        Console.WriteLine("1～4の数値を入力してください。");
+                        continue;
+                    }
+                    break;
+                }
+            }
         }
     }
 }
