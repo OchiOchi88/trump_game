a024a55e8f2cd27ad7c8ff6a2a75da820bfd3b15
OchiOchi88 <DESKTOP-GQ60U0C\student>
2025-06-20 12:08:12 +0900

    #2 対応中

--- trump_game/trump_game/Program.cs
+++ trump_game/trump_game/Program.cs
@@ -10,6 +10,12 @@ namespace trump_game
     {
         static void Main(string[] args)
         {
+            int[] number = { 0, 0, 0, 0 };
+            int numberHolder = 0;
+            int numberScanner = 0;
+            int[] pareHolder = { 0, 0, 0 };
+            int pareNumber = 0;
+
             //  カード番号を4回入力させる
             for (int i = 0; i < 4; i++)
             {
@@ -26,8 +32,72 @@ namespace trump_game
                         Console.WriteLine("1～4の数値を入力してください。");
                         continue;
                     }
-                    break;
+                    else
+                    {
+                        if (input >= 1 && input <= 4)
+                        {
+                            number[i] = input;
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("1～4の数値を入力してください。");
+                            continue;
+                        }
+                    }
                 }
+            }
+            Console.WriteLine("入力した番号は");
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write($"{number[j]} , ");
+            }
+            Console.WriteLine("\nです。");
+
+            for (int k = 0; k < 3; k++)
+            {
+                numberHolder = number[k];
+                for (int l = k + 1; l < 4; l++)
+                {
+                    numberScanner = number[l];
+                    if (numberHolder == numberScanner)
+                    {
+                        pareHolder[pareNumber] = numberHolder;
+                        pareNumber++;
+                        l = 4;
+                    }
+                }
+            }
+
+            Console.Write("結果は、");
+            if (pareHolder[0] != 0)
+            {
+                if (pareHolder[1] != 0)
+                {
+                    if (pareHolder[0] == pareHolder[1])
+                    {
+                        if (pareHolder[1] == pareHolder[2])
+                        {
+                            Console.Write("フォーカードです。");
+                        }
+                        else
+                        {
+                            Console.Write("スリーカードです。");
+                        }
+                    }
+                    else
+                    {
+                        Console.Write("ツーペアです。");
+                    }
+                }
+                else
+                {
+                    Console.Write("ワンペアです。");
+                }
+            }
+            else
+            {
+                Console.Write("ハイカードです。");
             }
         }
     }
